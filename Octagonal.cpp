0f660a8f41b8513b0b4ffbfe6de7344e1d06a162
Armin Abdollahi <103449830+Armin-Abdollahi@users.noreply.github.com>
2023-02-28 19:54:48 +0330

    Add files via upload

--- Octagonal.cpp
+++ Octagonal.cpp
@@ -1,4 +1,4 @@
-#include<glut.h>
+#include<GL/glut.h>
 void display(void)
 {
 	glClear(GL_COLOR_BUFFER_BIT);
@@ -23,7 +23,7 @@ int main(int argc, char **argv)
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE);
 	glutInitWindowSize(400, 500);
-	glutCreatWindow('Test');
+	glutCreateWindow("Test");
 	glClearColor(0, 0, 0, 0);
 	glutInitWindowPosition(50, 50);
 	glutDisplayFunc(display);
