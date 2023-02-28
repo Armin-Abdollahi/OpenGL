f820e2adb3122568a397f1b653207abe5c8df921
Armin Abdollahi <103449830+Armin-Abdollahi@users.noreply.github.com>
2023-02-28 20:13:01 +0330

    Add files via upload

---
+++ Lozenge.cpp
@@ -0,0 +1,28 @@
+#include<GL/glut.h>
+void display(void)
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	glPolygonMode(GL_FRONT_AND_BACK, GLU_LINE);
+	glBegin(GL_QUADS);
+	glColor3f(0.2, 0.8, 1);
+	glVertex2f(0, 0);
+	glVertex2f(-0.15, 0.4);
+	glVertex2f(-0.3, 0);
+	glVertex2f(-0.15, -0.4);
+
+
+	glEnd();
+
+	glutSwapBuffers();
+}
+int main(int argc, char **argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE);
+	glutInitWindowSize(400, 500);
+	glutCreateWindow("Test");
+	glClearColor(0, 0, 0, 0);
+	glutInitWindowPosition(50, 50);
+	glutDisplayFunc(display);
+	glutMainLoop();
+}
