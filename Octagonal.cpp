dda8996858533164ed69f76c4f2536b2e5a7638d
Armin Abdollahi <103449830+Armin-Abdollahi@users.noreply.github.com>
2023-02-28 19:28:21 +0330

    Add files via upload

---
+++ Octagonal.cpp
@@ -0,0 +1,31 @@
+#include<glut.h>
+void display(void)
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	glPolygonMode(GL_FRONT_AND_BACK, GLU_LINE);
+	glBegin(GL_POLYGON);
+	glColor3f(0.6, 1, 0.9);
+	glVertex2f(0.3,-0.6);
+	glVertex2f(0.6,-0.3);
+	glVertex2f(0.6,0.3);
+	glVertex2f(0.3,0.6);
+	glVertex2f(-0.3,0.6);
+	glVertex2f(-0.6,0.3);
+	glVertex2f(-0.6,-0.3);
+	glVertex2f(-0.3,-0.6);
+
+	glEnd();
+
+	glutSwapBuffers();
+}
+int main(int argc, char **argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE);
+	glutInitWindowSize(400, 500);
+	glutCreatWindow('Test');
+	glClearColor(0, 0, 0, 0);
+	glutInitWindowPosition(50, 50);
+	glutDisplayFunc(display);
+	glutMainLoop();
+}
