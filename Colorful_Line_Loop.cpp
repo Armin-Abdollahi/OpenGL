8d015e0aff7b1d3cdc1699e3f324b1f65869a875
Armin Abdollahi <103449830+Armin-Abdollahi@users.noreply.github.com>
2023-03-02 23:10:40 +0330

    Add files via upload

---
+++ Colorful_Line_Loop.cpp
@@ -0,0 +1,31 @@
+#include<GL/glut.h>
+void display(void)
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	glBegin(GL_LINE_LOOP);
+	glColor3f(0.8, 0.3, 0.6);
+	glVertex2f(-0.5, -0.5);
+
+	glColor3f(0, 0, 1);
+	glVertex2f(-0.5, 0);
+
+	glColor3f(0, 1, 0);
+	glVertex2f(0.5, 0);
+
+	glColor3f(1, 0, 0);
+	glVertex2f(0.5, 0.5);
+
+	glEnd();
+	glutSwapBuffers();
+}
+int main(int argc, char **argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE);
+	glutInitWindowSize(750, 700);
+	glutCreateWindow("Test");
+	glClearColor(0, 0, 0, 0);
+	glutInitWindowPosition(50, 50);
+	glutDisplayFunc(display);
+	glutMainLoop();
+}
