cc521473a8011b65ffa1df9a84746a7268880ac9
Armin Abdollahi <103449830+Armin-Abdollahi@users.noreply.github.com>
2023-03-03 16:34:14 +0330

    Add files via upload

---
+++ Dotted_Line.cpp
@@ -0,0 +1,39 @@
+#include<GL/glut.h>
+void display(void)
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	glColor3f(0.9, 0.2, 0.3);
+
+	GLushort stPattern = 0xAAAA;
+	glEnable(GL_LINE_STIPPLE);
+	glLineStipple(3, stPattern);
+	glLineWidth(3.8);
+	glBegin(GL_LINES);
+
+	glVertex2f(-0.6, 0.2);
+	glVertex2f(0.6, 0.2);
+	glEnd();
+
+	glColor3f(0.9, 0.2, 0.3);
+	glEnable(GL_LINE_STIPPLE);
+	glLineStipple(3, stPattern);
+	glLineWidth(3.8);
+	glBegin(GL_LINE_LOOP);
+
+	glVertex2f(-0.6, -0.2);
+	glVertex2f(0.6, -0.2);
+	glEnd();
+
+	glutSwapBuffers();
+}
+int main(int argc, char **argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE);
+	glutInitWindowSize(750, 700);
+	glutCreateWindow("Test");
+	glClearColor(0, 0, 0, 0);
+	glutInitWindowPosition(50, 50);
+	glutDisplayFunc(display);
+	glutMainLoop();
+}
